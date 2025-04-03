8d37ad4a8dbe80af753afac1c0362f3898488ab8
Dimitrije Radojkovic <dimitrijeradojkovic8@gmail.com>
2025-04-03 15:54:36 +0200

    sredjen bug

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home(props: {
 }) {
   const videos = await getVideos(0);
   const searchParams = await props?.searchParams;
-  const id = searchParams?.watch || videos[0].id;
+  const id = searchParams?.watch || videos[0]?.id || null;
 
   return (
     <div className="md:h-full h-[90%] w-full flex justify-center items-center bg-black">
@@ -18,7 +18,7 @@ export default async function Home(props: {
           if(v.id === id){
             return <VideoPage key={v.id} video={v} />
           }
-        }) : <VideoPage key={id} video={null} />) : <div className="h-full w-full text-5xl">No videos :(</div>
+        }) : <VideoPage key={id} video={null} />) : <div className="h-full w-full text-5xl text-white flex justify-center items-center">No videos :(</div>
       }
     </div>
   );
